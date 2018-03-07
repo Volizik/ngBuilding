f74de1f9e6e50471a0176e9fcdeec8b2ba282eb2
kavalar <apuc06@mail.ru>
2018-03-07 10:45:30 +0300

    hash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import { LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 import {AppComponent} from './app.component';
 import {MainComponent} from './main/main.component';
 import {InsideComponent} from './inside/inside.component';
 import {RouterModule, Routes} from '@angular/router';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 const routes: Routes = [
     {path: '', component: MainComponent},
@@ -24,7 +24,7 @@ const routes: Routes = [
         RouterModule.forRoot(routes),
         BrowserAnimationsModule
     ],
-    providers: [],
+    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
     bootstrap: [AppComponent]
 })
 export class AppModule {
